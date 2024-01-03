2116e2ec1511d536fc5dfc8694d6f13146453952
Diarrassouba ibrahim <diarrassoubaibra58@gmail.com>
2024-01-03 14:31:45 +0000

    initial commit

---
+++ hello.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+# def index():
+#     return "<h1>Hello World</h1>"
+
+def index():
+    return render_template('index.html')
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html', user_name=name)
+
+#invalid URL
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+# Internal server error
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template('500.html'), 500
